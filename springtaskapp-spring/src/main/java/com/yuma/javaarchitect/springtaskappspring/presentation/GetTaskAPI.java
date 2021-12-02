232994d152bb9fe6342b15f350d6a84c68637140
Yumapon <keropon1274@gmail.com>
2021-12-02 22:58:18 +0900

    add access dynamodb access repo

--- springtaskapp-spring/src/main/java/com/yuma/javaarchitect/springtaskappspring/presentation/GetTaskAPI.java
+++ springtaskapp-spring/src/main/java/com/yuma/javaarchitect/springtaskappspring/presentation/GetTaskAPI.java
@@ -1,12 +1,16 @@
 package com.yuma.javaarchitect.springtaskappspring.presentation;
 
 import java.util.List;
+import java.util.Optional;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.yuma.javaarchitect.springtaskappcore.domain.entity.Task;
 import com.yuma.javaarchitect.springtaskappcore.usecase.GetTaskUsecase;
+import com.yuma.javaarchitect.springtaskappspring.dynamo.APIUser;
 import com.yuma.javaarchitect.springtaskappspring.presentation.model.GetTaskAllResDto;
 import com.yuma.javaarchitect.springtaskappspring.presentation.model.GetTaskByIdResDto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,10 +30,21 @@ public class GetTaskAPI {
     @NonNull
     private final GetTaskUsecase usecase;
 
+    //DynamoDBのMapperClass
+    @Autowired
+    private final DynamoDBMapper mapper;
+
+    /**
+     * TODO コメント
+     * 
+     * @param id
+     * @param email
+     * @return
+     */
     @NonNull
     @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin
-    public GetTaskByIdResDto getTaskById(@NonNull @PathVariable("id") String id, @PathVariable("email") String email) {
+    public GetTaskByIdResDto getTaskById(@NonNull @PathVariable("id") String id, @RequestParam("email") String email) {
 
         System.out.println("email" + email);
 
@@ -47,18 +62,49 @@ public class GetTaskAPI {
 
     }
 
+    /**
+     * TODO コメント
+     * 
+     * @param email
+     * @return
+     */
     @NonNull
     @GetMapping(path = "/getall", produces = MediaType.APPLICATION_JSON_VALUE)
     @CrossOrigin
     public GetTaskAllResDto getTaskAll(@RequestParam("email") String email){
 
+        /*
         System.out.println("email:" + email);
+
+        System.out.println("処理開始");
+        
+        APIUser example = new APIUser("example@gmail.com", "admin");
+        repository.save(example);
+
+        Optional<APIUser> result = repository.findById("example@gmail.com");
+        if(result.isPresent())
+            System.out.println(result.get());
+        
+        repository.delete(example);
+        
+        System.out.println("処理終了");
 
         List<Task> taskList = usecase.invoke();
 
-        GetTaskAllResDto result = GetTaskAllResDto.builder().taskList(taskList).build();
+        GetTaskAllResDto result2 = GetTaskAllResDto.builder().taskList(taskList).build();
 
-        return result;
+        return result2;
+        */
+
+        //データ格納
+        mapper.save(APIUser.builder().email(email).role("guest").build());
+
+        //データ取り出し
+        String id = email;
+        APIUser user = mapper.load(APIUser.class, id);
+        System.out.println(user);
+
+        return null;
     }
 
 }
