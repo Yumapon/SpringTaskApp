b1378f14e62e964bd23508d8437460365e531f78
丘本悠真 <yuma@okamotoyuumanoMac-mini.local>
2022-01-17 17:11:28 +0900

    コンパイルエラーを解消

--- springtaskapp-spring/src/main/java/com/yuma/javaarchitect/springtaskappspring/config/AppConfig.java
+++ springtaskapp-spring/src/main/java/com/yuma/javaarchitect/springtaskappspring/config/AppConfig.java
@@ -1,9 +1,6 @@
 package com.yuma.javaarchitect.springtaskappspring.config;
 
 import com.yuma.javaarchitect.springtaskappcore.domain.entity.repository.TaskRepo;
-import com.yuma.javaarchitect.springtaskappcore.usecase.AddTaskUsecase;
-import com.yuma.javaarchitect.springtaskappcore.usecase.ChangeTaskUsecase;
-import com.yuma.javaarchitect.springtaskappcore.usecase.DeleteTaskUsecase;
 import com.yuma.javaarchitect.springtaskappcore.usecase.GetTaskUsecase;
 
 import org.springframework.context.annotation.Bean;
